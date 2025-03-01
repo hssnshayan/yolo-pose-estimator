2fc88e9047dbd5e5ab1ba7ccbf1a787e7d56bd22
Parsa Hashemkhany <parsahashemkhany.1385.pppp@gmail.com>
2025-03-01 05:16:04 -0800

    skeletons updated

--- skeletons/skeletons.py
+++ skeletons/skeletons.py
@@ -1,8 +1,8 @@
 SKELETON_ANGLE = {
     # "left_shoulder": [11, 5, 7],
     # "right_shoulder": [8, 6, 12],
-    # "left_elbow": [5, 7, 9],
-    # "right_elbow": [6, 8, 10],
+    "left_elbow": [5, 7, 9],
+    "right_elbow": [6, 8, 10],
     "left_hip": [5, 11, 13],
     "right_hip": [6, 12, 14],
     "left_knee": [11, 13, 15],

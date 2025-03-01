0dd16462d00d56c4b34cbef45aa606f85f75f380
Parsa Hashemkhany <parsahashemkhany.1385.pppp@gmail.com>
2025-03-01 04:44:38 -0800

    Skeletons file added

---
+++ skeletons/skeletons.py
@@ -0,0 +1,16 @@
+SKELETON_ANGLE = {
+    # "left_shoulder": [11, 5, 7],
+    # "right_shoulder": [8, 6, 12],
+    # "left_elbow": [5, 7, 9],
+    # "right_elbow": [6, 8, 10],
+    "left_hip": [5, 11, 13],
+    "right_hip": [6, 12, 14],
+    "left_knee": [11, 13, 15],
+    "right_knee": [12, 14, 16],
+}
+SKELETON_DISTANCE = {
+    "left_hip_to_ankle": [11, 15],
+    "right_hip_to_ankle": [12, 16],
+    "left_shoulder_to_knee": [5, 13],
+    "right_shoulder_to_knee": [6, 14],
+}
